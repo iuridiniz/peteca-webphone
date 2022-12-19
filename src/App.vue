f19d6f24b1bba61277dce030c386fcafcca2a5bd
Iuri Diniz <iuridiniz@gmail.com>
2022-12-19 16:26:18 -0300

    feat: enhance display

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import { ref, computed, watch } from 'vue';
 
 type AppState = 'online:idle' | 'call:incoming-ringing' | 'call:outgoing-active' | 'call:onhold' | 'offline';
 
-const displayContent = ref<string>("");
+const displayContent = ref<string>("8400001000");
 const AppState = ref<AppState>('offline');
 const inCall = computed<boolean>(() => AppState.value.startsWith('call:'));
 const displayHasContent = computed<boolean>(() => displayContent.value !== null && displayContent.value.length > 0);
@@ -35,7 +35,7 @@ const validateKeys = (e: KeyboardEvent) => {
 const keypadButtonClicked = (e: MouseEvent) => {
   const button = e.currentTarget as HTMLButtonElement;
   const value = button.dataset.value;
-  const input = document.querySelector(".pw-display-text") as HTMLInputElement;
+  const input = document.querySelector(".pw-display-middle") as HTMLInputElement;
   input.focus();
   displayContent.value += value;
 }
@@ -49,7 +49,7 @@ const callEndButtonClicked = () => {
 }
 
 const clearButtonClicked = () => {
-  const input = document.querySelector(".pw-display-text") as HTMLInputElement;
+  const input = document.querySelector(".pw-display-middle") as HTMLInputElement;
   input.focus();
   displayContent.value = "";
 }
@@ -57,19 +57,19 @@ const clearButtonClicked = () => {
 watch(
   [displayContent],
   () => {
-    const input = document.querySelector(".pw-display-text") as HTMLInputElement;
+    const input = document.querySelector(".pw-display-middle") as HTMLInputElement;
 
-    if (!input.classList.contains("pw-display-text-small")) {
+    if (!input.classList.contains("pw-display-middle-small")) {
       if (input.clientWidth < input.scrollWidth) {
-        input.classList.add("pw-display-text-small");
+        input.classList.add("pw-display-middle-small");
         input.dataset.overflowOnLenght = input.value.length.toString();
         return;
       }
     } else {
-      if (input.classList.contains("pw-display-text-small") && input.dataset.overflowOnLenght) {
+      if (input.classList.contains("pw-display-middle-small") && input.dataset.overflowOnLenght) {
         const overflowOnLenght = parseInt(input.dataset.overflowOnLenght as string);
         if (input.value.length < overflowOnLenght) {
-          input.classList.remove("pw-display-text-small");
+          input.classList.remove("pw-display-middle-small");
           input.dataset.overflowOnLenght = "";
         }
       }
@@ -82,30 +82,58 @@ watch(
 </script>
 
 <template>
+  <aside style="display: none">
+    <svg xmlns="http://www.w3.org/2000/svg">
+      <symbol id="pw-svg-icon-minimize" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24" width="1em"
+        height="1em">
+        <path fill="currentColor" d="M19 13H5v-2h14Z" />
+      </symbol>
+      <symbol id="pw-svg-icon-settings" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24" height="1em">
+        <path fill="currentColor"
+          d="m9.25 22l-.4-3.2q-.325-.125-.612-.3q-.288-.175-.563-.375L4.7 19.375l-2.75-4.75l2.575-1.95Q4.5 12.5 4.5 12.337v-.675q0-.162.025-.337L1.95 9.375l2.75-4.75l2.975 1.25q.275-.2.575-.375q.3-.175.6-.3l.4-3.2h5.5l.4 3.2q.325.125.613.3q.287.175.562.375l2.975-1.25l2.75 4.75l-2.575 1.95q.025.175.025.337v.675q0 .163-.05.338l2.575 1.95l-2.75 4.75l-2.95-1.25q-.275.2-.575.375q-.3.175-.6.3l-.4 3.2Zm2.8-6.5q1.45 0 2.475-1.025Q15.55 13.45 15.55 12q0-1.45-1.025-2.475Q13.5 8.5 12.05 8.5q-1.475 0-2.488 1.025Q8.55 10.55 8.55 12q0 1.45 1.012 2.475Q10.575 15.5 12.05 15.5Z" />
+      </symbol>
+    </svg>
+  </aside>
   <div class="pw pw-root">
     <main class="pw-main">
       <div class="pw-app-bar">
         <div class="pw-app-bar-title">
           Peteca WebPhone
         </div>
-        <div class="pw-app-bar-status">•</div>
         <div class="pw-app-bar-actions">
           <button class="pw-app-bar-action-button">
-            <svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24" width="1em">
-              <path fill="currentColor" d="M6 21v-2h12v2Z" />
+            <svg class="pw-icon-minimize" width="1em" height="1em">
+              <use xlink:href="#pw-svg-icon-minimize" />
             </svg>
           </button>
           <button class="pw-app-bar-action-button">
-            <svg class="pw-icon-tape-line" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid meet"
-              viewBox="0 0 24 24" width="1em">
-              <path fill="currentColor"
-                d="m9.25 22l-.4-3.2q-.325-.125-.612-.3q-.288-.175-.563-.375L4.7 19.375l-2.75-4.75l2.575-1.95Q4.5 12.5 4.5 12.337v-.675q0-.162.025-.337L1.95 9.375l2.75-4.75l2.975 1.25q.275-.2.575-.375q.3-.175.6-.3l.4-3.2h5.5l.4 3.2q.325.125.613.3q.287.175.562.375l2.975-1.25l2.75 4.75l-2.575 1.95q.025.175.025.337v.675q0 .163-.05.338l2.575 1.95l-2.75 4.75l-2.95-1.25q-.275.2-.575.375q-.3.175-.6.3l-.4 3.2Zm2.8-6.5q1.45 0 2.475-1.025Q15.55 13.45 15.55 12q0-1.45-1.025-2.475Q13.5 8.5 12.05 8.5q-1.475 0-2.488 1.025Q8.55 10.55 8.55 12q0 1.45 1.012 2.475Q10.575 15.5 12.05 15.5Z" />
+            <svg class="pw-icon-minimize" width="1em" height="1em">
+              <use xlink:href="#pw-svg-icon-settings" />
             </svg>
           </button>
         </div>
       </div>
       <div class="pw-display">
-        <input @keypress="validateKeys" v-model="displayContent" type="text" class="pw-display-text" />
+        <div class="pw-display-top">
+          <div class="pw-display-top-left">00:00</div>
+          <div class="pw-display-top-center"></div>
+          <div class="pw-display-top-right">
+            <svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24" width="1em">
+              <path fill="currentColor"
+                d="M12 10C10.9 10 10 10.9 10 12S10.9 14 12 14 14 13.1 14 12 13.1 10 12 10M18 12C18 8.7 15.3 6 12 6S6 8.7 6 12C6 14.2 7.2 16.1 9 17.2L10 15.5C8.8 14.8 8 13.5 8 12.1C8 9.9 9.8 8.1 12 8.1S16 9.9 16 12.1C16 13.6 15.2 14.9 14 15.5L15 17.2C16.8 16.2 18 14.2 18 12M12 2C6.5 2 2 6.5 2 12C2 15.7 4 18.9 7 20.6L8 18.9C5.6 17.5 4 14.9 4 12C4 7.6 7.6 4 12 4S20 7.6 20 12C20 15 18.4 17.5 16 18.9L17 20.6C20 18.9 22 15.7 22 12C22 6.5 17.5 2 12 2Z" />
+            </svg>
+          </div>
+        </div>
+        <input class="pw-display-middle pw-display-text-color-neutral" @keypress="validateKeys" v-model="displayContent"
+          type="text" />
+        <div class="pw-display-bottom">
+          <div class="pw-display-bottom-left">9000@sip.domain.com</div>
+          <div class="pw-display-bottom-center"></div>
+          <div class="pw-display-bottom-right">
+            <span class="pw-display-text-color-good">•online</span>
+            <span class="pw-display-text-color-bad">•offline</span>
+          </div>
+        </div>
       </div>
       <div class="pw-keypad">
         <button @click="keypadButtonClicked" data-value="1" class="pw-keypad-button pw-keypad-button-1">
@@ -192,6 +220,8 @@ watch(
 
 <style lang="scss">
 .pw {
+
+
   .pw-main {
     @apply pw-bg-gray-100;
     @apply pw-flex-col;
@@ -211,8 +241,10 @@ watch(
   }
 
   .pw-display {
-    @apply pw-bg-gray-600;
-    @apply pw-py-8;
+    @apply pw-bg-gray-800;
+    @apply pw-text-gray-100;
+
+    @apply pw-py-2;
     @apply pw-px-4;
     @apply pw-max-w-sm;
     @apply pw-rounded-xl;
@@ -220,19 +252,74 @@ watch(
     @apply pw-m-2;
     @apply pw-h-24;
     @apply pw-flex;
-  }
-
-  .pw-display-text {
+    @apply pw-flex-col;
+    @apply pw-justify-between;
+    @apply pw-items-stretch;
     font-family: "Peteca", ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace;
     ;
-    @apply pw-text-center;
-    @apply pw-text-green-300;
-    @apply pw-bg-gray-600;
-    @apply pw-w-full;
-    @apply pw-text-4xl;
   }
 
-  .pw-display-text.pw-display-text-small {
+  .pw-display-text-color-neutral {
+    @apply pw-text-gray-100;
+  }
+
+  .pw-display-text-color-good {
+    @apply pw-text-green-400;
+  }
+
+  .pw-display-text-color-bad {
+    @apply pw-text-red-400;
+  }
+
+  .pw-display-top,
+  .pw-display-bottom {
+    @apply pw-mx-1;
+    @apply pw-columns-3;
+    @apply pw-text-xs;
+    @apply pw-overflow-clip;
+
+  }
+
+  .pw-display-top-left,
+  .pw-display-bottom-left {
+    @apply pw-flex;
+    @apply pw-justify-start;
+    @apply pw-text-left;
+    @apply pw-px-0;
+    min-height: 1em;
+    min-width: 1em;
+  }
+
+  .pw-display-top-center,
+  .pw-display-bottom-center {
+    @apply pw-flex;
+    @apply pw-justify-center;
+    @apply pw-text-center;
+    @apply pw-px-0;
+    min-height: 1em;
+    min-width: 1em;
+  }
+
+  .pw-display-top-right,
+  .pw-display-bottom-right {
+    @apply pw-flex;
+    @apply pw-justify-end;
+    @apply pw-text-right;
+    @apply pw-px-0;
+    min-height: 1em;
+    min-width: 1em;
+  }
+
+  .pw-display-middle {
+
+    @apply pw-text-center;
+    @apply pw-w-full;
+    @apply pw-text-4xl;
+    @apply pw-grow;
+    background-color: inherit;
+  }
+
+  .pw-display-middle.pw-display-middle-small {
     @apply pw-text-base;
   }
 
@@ -322,7 +409,6 @@ watch(
     @apply pw-text-gray-200;
     @apply pw-p-0;
     @apply pw-mx-1;
-
     @apply pw-rounded-full;
     @apply pw-shadow-lg;
 
